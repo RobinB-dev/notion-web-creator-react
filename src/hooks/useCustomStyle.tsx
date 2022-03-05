f3d1de366c647f2d13ec26716c4207ce94fc5e36
Rob1 <robin.bourachot@hetic.net>
2022-03-05 20:29:00 +0100

    :art: Refactoring useCustomStyle

--- src/hooks/useCustomStyle.tsx
+++ src/hooks/useCustomStyle.tsx
@@ -24,112 +24,61 @@ const useCustomStyle = (blockObj: string, blockId: string) => {
 
 
     const TypeStyle = {
-        text: [
-            ["fontFamily", dataCtx.fontFamily],
-            ["textColor", dataCtx.textColor],
-            ["theme", dataCtx.theme]],
-        block: [
-            ["borderRadius", dataCtx.borderRadius]],
-        image: [
-            ["borderRadius", dataCtx.borderRadius],
-            ["theme", dataCtx.theme]],
-        general: [
-            ["fontFamily", dataCtx.fontFamily],
-            ["textColor", dataCtx.textColor],
-            ["theme", dataCtx.theme]]
+        text: ["fontFamily", "textColor", "theme"],
+        block: ["borderRadius"],
+        image: ["borderRadius", "theme"],
+        general: ["fontFamily", "textColor", "theme"]
     }
 
-    const checkIfKeyExist = (obj: String[], key: string) => {
-        for (let i = 0; i < obj.length; i++) {
-            if (testObj(obj[i], key) !== undefined) {
-                return i
+    // update hook state from the context
+    const UpdateHookStates = (styleName: string, styleCtx: any, _blockObj: string) => {
+        if (!styleCtx[_blockObj]) { return } // select only the good type of block
+        setCustomStyle((prevState) => ({
+            ...prevState,
+            [styleName]: styleCtx[_blockObj]
+        }))
+    }
+
+    const UpdateStyleStore = (styleName: string, styleCtx: any, _id: string, _blockObj: string) => {
+        let newStyle = { id: _id, [styleName]: styleCtx[_blockObj] };
+        let newPageStyle = { id: _id, [_blockObj]: { [styleName]: styleCtx[_blockObj] } };
+        let newPageStyle2 = { [styleName]: styleCtx[_blockObj] };
+
+        if (acitveBlockId === 'a0c1294e-page') {
+            if (!styleCtx[_blockObj]) { return } // select only the good type of block
+
+            if (obj === undefined) {
+                dataCtx.setStyleStore([...dataCtx.styleStore, newPageStyle])
+            } else if (testObj(obj, _blockObj) === undefined) {
+                console.log("new styleName");
+                obj[_blockObj] = newPageStyle2
+            } else {
+                obj[_blockObj][styleName] = styleCtx[_blockObj]
+            }
+
+        } else {
+            if (obj === undefined) {
+                dataCtx.setStyleStore([...dataCtx.styleStore, newStyle])
+            } else {
+                obj[styleName] = styleCtx[_blockObj]
             }
         }
-        return false
     }
 
     const updateType = (_type: string, _id: string, _blockObj: string) => {
-
         const elemType = testObj(TypeStyle, _type)
 
         for (let i = 0; i < elemType.length; i++) {
-            const styleName = elemType[i][0];
-            const styleCtx = elemType[i][1];
+            const styleName = elemType[i];
+            const styleCtx = dataCtx[styleName];
 
-            // if the value is not in the context
+            // if the context is not empty
             if (styleCtx !== "") {
-                let newStyle = { id: _id, [styleName]: styleCtx[_blockObj] };
-                // if a id doesnt exist in styleStore
-
-                // let newPageStyle = { id: _id, [_blockObj]: { [styleName]: styleCtx[_blockObj] } };
-                let newPageStyle = { id: _id };
-                let newPageStyle2 = { [styleName]: styleCtx[_blockObj] };
-                // handle style of the page
-                if (acitveBlockId === 'a0c1294e-page') {
-
-                    // select only the good type of block
-                    if (styleCtx[_blockObj]) {
-
-                        // store the style in the styleStore
-                        if (obj === undefined) {
-                            console.log("not stored");
-                            dataCtx.setStyleStore([...dataCtx.styleStore, newPageStyle])
-                        } else if (testObj(obj, _blockObj) === undefined) {
-                            console.log("stored");
-                            obj[_blockObj] = newPageStyle2
-                        } else if (testObj(obj, _blockObj).length === undefined) {
-                            if (testObj(obj, _blockObj)[styleName] === undefined) {
-                                console.log("stored2_new");
-                                obj[_blockObj] = [obj[_blockObj], newPageStyle2]
-                            } else {
-                                console.log("stored2_reset");
-                                obj[_blockObj] = newPageStyle2
-                            }
-                        } else {
-                            console.log("stored_many");
-
-                            const isExisting = checkIfKeyExist(obj[_blockObj], styleName)
-                            if (isExisting === false) {
-                                console.log(isExisting, styleName);
-                                console.log("stored3_uptzde");
-                                obj[_blockObj] = [...obj[_blockObj], newPageStyle2]
-                            } else {
-                                console.log(isExisting, styleName);
-                                obj[_blockObj][isExisting] = newPageStyle2
-                            }
-                            console.log(testObj(obj, _blockObj), styleName);
-
-                        }
-
-                        // update hook state from the styleStore
-                        setCustomStyle((prevState) => ({
-                            ...prevState,
-                            [styleName]: styleCtx[_blockObj]
-                        }))
-                    }
-
-                } else { // handle style of one block
-
-                    // store the style in the styleStore
-                    if (obj === undefined) {
-                        dataCtx.setStyleStore([...dataCtx.styleStore, newStyle])
-                    } else {
-                        obj[styleName] = styleCtx[_blockObj]
-                    }
-
-                    // update hook state from the styleStore
-                    setCustomStyle((prevState) => ({
-                        ...prevState,
-                        [styleName]: styleCtx[_blockObj]
-                    }))
-                }
-
-                // console.log(dataCtx);
+                UpdateStyleStore(styleName, styleCtx, _id, _blockObj)
+                UpdateHookStates(styleName, styleCtx, _blockObj)
 
                 // function detect style conflicts
-
             }
-
         }
     }
 
@@ -138,19 +87,14 @@ const useCustomStyle = (blockObj: string, blockId: string) => {
         const updateStyleStore = () => {
 
             if (acitveBlockId === 'a0c1294e-page') {
-                // console.log('page ', activeBlockType);
                 updateType(activeBlockType, acitveBlockId, blockObj)
-
             } else if (acitveBlockId === blockId) {
-                // console.log('activeBlockType ', activeBlockType, blockObj);
                 if (activeBlockType === "text" || activeBlockType === "image") {
                     updateType(activeBlockType, acitveBlockId, "select")
                 } else {
                     console.log('not text');
                 }
             }
-
-            // console.log("refresh ", dataCtx.styleStore);
         }
 
         updateStyleStore()
